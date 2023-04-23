da8bec3aa731832adb8305759b3d6d462057929b
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2023-04-22 23:43:04 -0400

    Implementation

--- diceAverage.py
+++ diceAverage.py
@@ -2,3 +2,21 @@
 # loop through and add each possible dice pair combination
 # Choose the lowest value in each pair
 # average the result
+
+rollsDict = []
+
+for i in range(20):
+  for j in range(20):
+    rollsDict.append((i + 1, j + 1))
+
+rollsDictPruned = []
+
+for x in rollsDict:
+  rollsDictPruned.append(min(x))
+
+cumul = 0
+for x in rollsDictPruned:
+  cumul += x
+
+cumul /= len(rollsDictPruned)
+print(cumul)
